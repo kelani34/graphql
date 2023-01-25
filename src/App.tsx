794a6e7d03f56cda54d1a58bef22d9dcd50fa260
kelani34 <kelanitaiwo34@gmail.com>
2023-01-26 00:38:31 +0100

    basic imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-   <></>
+    <div>
+      <></>
+    </div>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+} from "@apollo/client";
 
+let client = new ApolloClient({
+  uri: "",
+  cache: new InMemoryCache(),
+});
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
