d217ebbf9c8066ba08769e9ccd31e75d6bb70db8
kelani34 <kelanitaiwo34@gmail.com>
2023-01-26 15:03:33 +0100

    request data from graphene backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
-import { useQuery, gql } from "@apollo/client";
+import { useQuery, gql, useMutation } from "@apollo/client";
 import { Customer } from "./Types";
-const GET_DATA = gql`
+
+const GET_CUSTOMERS = gql`
   {
     customers {
       id
@@ -11,16 +12,87 @@ const GET_DATA = gql`
     }
   }
 `;
+
+const MUTATE_CUSTOMERS = gql`
+  mutation addCustomer($name: String!, $industry: String!) {
+    createCustomer(name: $name, industry: $industry) {
+      customer {
+        id
+        name
+        industry
+      }
+    }
+  }
+`;
 function App() {
-  const { loading, error, data } = useQuery(GET_DATA);
+  const { loading, error, data } = useQuery(GET_CUSTOMERS);
+  const [
+    createCustomer,
+    {
+      loading: createCustomerLoading,
+      error: createCustomerError,
+      data: createCustomerData,
+    },
+  ] = useMutation(MUTATE_CUSTOMERS, {
+    refetchQueries: [
+      {
+        query: GET_CUSTOMERS,
+      },
+    ],
+  });
+
+  const [name, setName] = useState<string>("");
+  const [industry, setIndustry] = useState<string>("");
 
   useEffect(() => {
     console.log("data", data, "error", error, "loading", loading);
-  });
+    console.log(
+      "create customer",
+      createCustomer,
+      "create customer data",
+      createCustomerData,
+      "create customer loading",
+      createCustomerLoading,
+      "create customer error",
+      createCustomerError
+    );
+  }, []);
   return (
     <>
       {error ? <p>Somrthing Went wrong</p> : null}
       {loading ? <p>Loading ...</p> : null}
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          createCustomer({ variables: { name: name, industry: industry } });
+        }}
+      >
+        <div>
+          <label htmlFor="name">Name: </label>
+          <input
+            id="name"
+            type="text"
+            value={name}
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+          />
+        </div>
+        <div>
+          <label htmlFor="industry">Industry: </label>
+          <input
+            id="industry"
+            type="text"
+            value={industry}
+            onChange={(e) => {
+              setIndustry(e.target.value);
+            }}
+          />
+        </div>
+        <button disabled={createCustomerLoading ? true : false} type="submit">
+          Submit
+        </button>
+      </form>
       <div>
         {data
           ? data.customers.map((customer: Customer) => {
